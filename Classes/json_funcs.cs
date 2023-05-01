3797fd3fee8fb2d5e5438308e779a011bbe5bf78
LeKeu <lekeuffer@gmail.com>
2023-05-01 17:43:07 -0300

    inserir dados no arq json EM FORMATO DE DICIONARIO

--- Classes/json_funcs.cs
+++ Classes/json_funcs.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,20 +12,30 @@ namespace projeto_integrado.Classes
 {
     internal class json_funcs
     {
-        public static void Convert_to_json(List<object> values)
+        public static void Convert_to_json(List<string> dados, string nome_tabela)
         {
 
             string arq_path = @"C:\Users\Lenovo\Desktop\JSONTESTE.json";
-
             // ler todos os dados que o arq contém
             var jsonData = System.IO.File.ReadAllText(arq_path);
-
             // des-serializar o arquivo e guardar em uma lista
             var dados_salvos = JsonConvert.DeserializeObject<List<object>>(jsonData);
 
-            // adicionar, no arq des-serializado os dados recentes
-            dados_salvos.Add(values);
+            Dictionary<string, string> dict_dados = new Dictionary<string, string>();
 
+            var values_colunas = conexao_api.ReadTableColumnName("Membro");
+            dict_dados.Add("Tabela", "Membro");
+            foreach (var row in values_colunas)
+            {
+                for (int i = 0; i < row.Count; i++)
+                {
+                    dict_dados.Add((string)row[i], dados[i]);
+                }
+            }
+
+
+            // adicionar, no arq des-serializado os dados recentes
+            dados_salvos.Add(dict_dados);
             // serializar o arq novo para salvar novamente
             var json_arq = JsonConvert.SerializeObject(dados_salvos);
             System.IO.File.WriteAllText(arq_path, json_arq);
@@ -44,17 +55,23 @@ namespace projeto_integrado.Classes
         public static void Read_from_json()  // lendo do arq json criado
         {
             string arq_path = @"C:\Users\Lenovo\Desktop\JSONTESTE.json";  // pegar caminho automatico!
-            string arq_lido = System.IO.File.ReadAllText(arq_path);  // leitura do arq
+            
+            dynamic jsonArq = JsonConvert.DeserializeObject(File.ReadAllText(arq_path));
+            List<object> aaa = new List<object>();
 
-            var nsei = Newtonsoft.Json.JsonConvert.DeserializeObject<List<object>>(arq_lido);
-
-            foreach (object o in nsei)
+            Console.WriteLine(jsonArq.Count);
+            foreach ( var item in jsonArq )
             {
-                Console.WriteLine(o.ToString());
+                aaa.Add(item);
             }
 
+            Console.WriteLine(aaa[0]);
+            Console.WriteLine(aaa[1]);
+
+            Console.WriteLine(aaa[2].GetType());
+
             //return(nsei);
-            
+
         }
     }
 }
